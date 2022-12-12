b94eff93cd7a74448b7e1156007104690767289c
lollobianco <loreb2002@gmail.com>
2022-12-12 13:35:27 +0100

    Creato paragrafo con conteggio caratteri

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php  
+
+   $paragraph = " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde maiores hic consequatur animi laborum dolorum eligendi ipsam minima earum inventore, maxime laboriosam quisquam fugit. Aut, possimus! Nihil mollitia ducimus placeat? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, recusandae beatae! Veritatis magnam in quod hic aperiam libero dolores, ratione unde, nostrum consectetur recusandae adipisci a alias nobis aliquid vel. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates commodi eius architecto eligendi natus, animi, quisquam veniam laboriosam sed ab dicta minima officiis libero vitae at et, expedita exercitationem eaque. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde maiores hic consequatur animi laborum dolorum eligendi ipsam minima earum inventore, maxime laboriosam quisquam fugit. Aut, possimus! Nihil mollitia ducimus placeat? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, recusandae beatae! Veritatis magnam in quod hic aperiam libero dolores, ratione unde, nostrum consectetur recusandae adipisci a alias nobis aliquid vel. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates commodi eius architecto eligendi natus, animi, quisquam veniam laboriosam sed ab dicta minima officiis libero vitae at et, expedita exercitationem eaque. "
+
+?>
+
+<!DOCTYPE html>
+<html lang="">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>php-badwords</title>
+</head>
+<body>
+   
+
+   <p> <?php echo var_dump($paragraph) ?> </p>
+
+
+</body>
+</html>
